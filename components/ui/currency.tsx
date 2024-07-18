3159aac668911e79a574642442e3e4b9a2a37dff
rajatmungra <102897093+rajatmungra@users.noreply.github.com>
2024-07-18 10:31:21 +0530

    Update currency.tsx

--- components/ui/currency.tsx
+++ components/ui/currency.tsx
@@ -1,9 +1,9 @@
 "use client"
 import { useState, useEffect } from 'react';
 
-const formatter = new Intl.NumberFormat('en-US', {
+const formatter = new Intl.NumberFormat('en-IN', {
     style: 'currency',
-    currency: 'USD'
+    currency: 'INR'
 })
 
 interface CurrencyProps {
